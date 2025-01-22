b4b0cc8ecc63fed27b032d0d5761280e26b152f5
jchuang1977 <jchuang1977@gmail.com>
2025-01-22 21:40:23 +0800

    Update xiaohongshu_model.py

--- xiaohongshu_model.py
+++ xiaohongshu_model.py
@@ -1,5 +1,5 @@
-from langchain_core.pydantic_v1 import BaseModel, Field
-#from pydantic import BaseModel,Field
+#from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel,Field
 from typing import List
 
 class Xiaohongshu(BaseModel):
